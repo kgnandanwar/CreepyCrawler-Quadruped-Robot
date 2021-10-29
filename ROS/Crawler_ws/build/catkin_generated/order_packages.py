56ea3dbe4f29af12aeb680ca399416480b23cb16
malpurepiyush07@gmail.com <malpurepiyush07@gmail.com>
2021-10-28 21:30:34 -0400

    Created ROS workspace

---
+++ ROS/Crawler_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/piyush/BlackHole/WPI/Robot Dynamics/Project/CreepyCrawler-Quadruped-Robot/ROS/Crawler_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/piyush/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/piyush/catkin_ws/devel;/opt/ros/noetic' != '' else []
